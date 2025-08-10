b3ac35aff8c4e2611a02c10cc79f9655e24f608c
eloydsdlh <e.desande.2021@alumnos.urjc.es>
2025-08-10 20:50:47 +0200

    Crear repositorio para Admin, Cita, Doctor, Paciente

---
+++ backend/src/main/java/com/centromedico/backend/repository/RepositorioDoctor.java
@@ -0,0 +1,10 @@
+package com.centromedico.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioDoctor extends JpaRepository<Doctor, Long> {
+    
+
+}
---
+++ backend/src/main/java/com/centromedico/backend/repository/RepositorioPaciente.java
@@ -0,0 +1,11 @@
+package com.centromedico.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioPaciente extends JpaRepository<Paciente, Long> {
+    
+    
+
+}
